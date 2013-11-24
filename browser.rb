356e5be1aac4e4ebc6385dd4bff321b996e3bcbc
John Moon <johnmoonyy@gmail.com>
2013-11-24 15:29:30 -0500

    Feature to search in StackOverFlow and Google was added

--- browser.rb
+++ browser.rb
@@ -1,32 +1,66 @@
 require 'uri'
 require 'time'
+require 'pry'
 
 class Browser
-  def initialize(question)
+  
+  def initialize(site,question)
+    @site = site
     @question = question
   end
 
-  def browse 
-    system("open", url) 
+  def browse_google 
+    system("open", google) 
   end
-  def url
+
+  def browse_sof
+    system("open", sof) 
+  end
+
+  def google
     ("http://www.google.com/search?q=#{URI.escape(@question)}")
   end
+
+  def sof
+    ("http://www.stackoverflow.com/search?q=#{URI.escape(@question)}")
+  end
+
+  def which_site
+  end
+
+  def question_input
+  end
+
+
 end
 
+    puts "Would you like to search Google, StackOverFlow(SOF), or both?"
+    print "> "
+    site = gets.chomp 
 
-puts "What quesiton do you have?"
-question = gets.chomp
-browser = Browser.new(question)
-browser.browse
+    puts "What quesiton do you have?"
+    print "> "
+    question_input = gets.chomp
+
+    browser = Browser.new(site, question_input)
+    if site.downcase == "google"
+      browser.browse_google
+    elsif site.downcase =="both"
+      browser.browse_google
+      browser.browse_sof
+    elsif site.downcase =="sof"
+      browser.browse_sof
+    else 
+      puts "You entered a wrong site"
+      exit
+    end
 
 puts "Did you find the answer?"
 answer = gets.chomp
 
-
 File.open('stats.txt', 'a+') do |log|
   log.puts "\n"
-  log.puts "Question '#{question}'"
+  log.puts "Question '#{@question}'"
   log.print "Answer: '#{answer}'"
 end
 
