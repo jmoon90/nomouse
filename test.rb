d6136b567a8cb2d6fbe798a1dd7b9cd0706da619
John Moon <johnmoonyy@gmail.com>
2013-12-08 08:43:05 -0500

    Changed the To-Do list. NoMouse will not be updating anymore

--- test.rb
+++ test.rb
@@ -7,13 +7,10 @@ array = [type]
 
 puts "What folder should we look at?"
 search_folder = gets.chomp
-no_string = search_folder.gsub(/"/, "") 
 
-jpg_file_paths = []
-
-  binding.pry
-Find.find('/Users/Sungmoon/#{array[0]}') do |path|
+jpg_file_paths = [] 
+Find.find("/Users/Sungmoon/#{search_folder}") do |path|
   jpg_file_paths << path if path =~/.*\.#{type}$/i
   end
-
+  
 
